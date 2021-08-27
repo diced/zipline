b5e882f07e146296bb6ae6cce244ce521330ac51
Nguyen Thanh Quang <admin@xwork.space>
2021-08-28 05:33:32 +0700

    feat(themes): added dracula theme (#92)
    * added dracula theme
    
    * change border color from white
    
    Co-authored-by: dicedtomato <35403473+diced@users.noreply.github.com>

---
+++ src/lib/themes/dracula.ts
@@ -0,0 +1,17 @@
+// https://github.com/AlphaNecron/
+// https://github.com/dracula/dracula-theme
+import createTheme from '.';
+
+export default createTheme({
+  type: 'dark',
+  primary: '#BD93F9',
+  secondary: '#6272A4',
+  error: '#FF5555',
+  warning: '#FFB86C',
+  info: '#8BE9FD',
+  border: '#7D8096',
+  background: {
+    main: '#282A36',
+    paper: '#44475A'
+  }
+});
