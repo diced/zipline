67ff93e640acf3d1308bb71279ecaf9adddef29b
diced <pranaco2@gmail.com>
2022-08-26 20:38:31 -0700

    fix: remove useless webpack cofnig

--- next.config.js
+++ next.config.js
@@ -11,14 +11,6 @@ module.exports = {
       },
     ];
   },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false, // the solution
-    };
-
-    return config;
-  },
   poweredByHeader: false,
   reactStrictMode: true,
 };
