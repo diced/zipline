d76e6444e0d7540dab93e72f44c31002937f28b0
diced <pranaco2@gmail.com>
2022-11-24 14:34:24 -0800

    fix: dumb cors headers

--- src/lib/middleware/withZipline.ts
+++ src/lib/middleware/withZipline.ts
@@ -65,6 +65,7 @@ export const withZipline =
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Content-Allow-Methods', api_config.methods.join(','));
     res.setHeader('Access-Control-Max-Age', '86400');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
     if (req.method === 'OPTIONS') return res.status(204).end();
 
