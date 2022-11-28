c967afc75cf93b746f79c54b9c1fdea92c2dbba4
dicedtomato <35403473+diced@users.noreply.github.com>
2022-11-27 18:12:27 -0800

    Update validateConfig.ts

--- src/lib/config/validateConfig.ts
+++ src/lib/config/validateConfig.ts
@@ -221,6 +221,16 @@ export default function validate(config): Config {
         if (errors.length) throw { errors };
         break;
       }
+      case 'supabase': {
+        const errors = [];
+        
+        if (!validated.datasource.supabase.key) errors.push('datasource.supabase.key is a required field');
+        if (!validated.datasource.supabase.url) errors.push('datasource.supabase.url is a required field');
+        if (!validated.datasource.supabase.bucket) errors.push('datasource.supabase.bucket is a required field');
+        if (errors.length) throw { errors };
+        
+        break;
+      }
     }
 
     return validated as unknown as Config;
