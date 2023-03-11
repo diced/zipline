37b208c815f4b75861d67939a73e01d487edd29b
TacticalCoderJay <gogojayvin923@gmail.com>
2023-03-11 12:55:54 -0800

    fix: allow admin removal

--- src/pages/api/user/[id].ts
+++ src/pages/api/user/[id].ts
@@ -79,6 +79,7 @@ async function handler(req: NextApiReq, res: NextApiRes, user: UserExtended) {
       return res.forbidden('cannot modify administrator');
 
     logger.debug(`attempting to update user ${id} with ${JSON.stringify(req.body)}`);
+    logger.debug(`user ${id} has ${!req.body.administrator} in administrator`);
 
     if (req.body.password) {
       const hashed = await hashPassword(req.body.password);
@@ -88,7 +89,7 @@ async function handler(req: NextApiReq, res: NextApiRes, user: UserExtended) {
       });
     }
 
-    if (req.body.administrator) {
+    if (typeof req.body.administrator != 'undefined') {
       await prisma.user.update({
         where: { id: target.id },
         data: { administrator: req.body.administrator },
