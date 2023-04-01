aa6fd937da732cda2028c8c2806316fb323e6959
TacticalCoderJay <gogojayvin923@gmail.com>
2023-04-01 00:09:53 -0700

    fix: apply new foreign key constraints to existing data

--- prisma/migrations/20230401040146_add_user_uuid/migration.sql
+++ prisma/migrations/20230401040146_add_user_uuid/migration.sql
@@ -5,17 +5,29 @@
   - The required column `uuid` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
 
 */
--- DropForeignKey
-ALTER TABLE "OAuth" DROP CONSTRAINT "OAuth_userId_fkey";
+-- Rename old foreign key
+ALTER TABLE "OAuth" RENAME CONSTRAINT "OAuth_userId_fkey" TO "OAuth_userId_old_fkey";
 
--- AlterTable
-ALTER TABLE "OAuth" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+-- Rename old column
+ALTER TABLE "OAuth" RENAME COLUMN "userId" TO "userId_old";
 
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "cuid" TEXT NOT NULL;
+-- Add new foreign key
+ALTER TABLE "OAuth" ADD CONSTRAINT "OAuth_userId_fkey";
 
--- CreateIndex
-CREATE UNIQUE INDEX "User_uuid_key" ON "User"("cuid");
+-- Add new column
+ALTER TABLE "OAuth" ADD COLUMN "userId" TEXT NOT NULL;
 
--- AddForeignKey
+-- Create index
+CREATE UNIQUE INDEX "User_cuid_key" ON "User"("cuid");
+
+-- Add foreign key
 ALTER TABLE "OAuth" ADD CONSTRAINT "OAuth_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("cuid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- Update userId with cuid
+UPDATE "OAuth" SET "userId" = "User"."cuid" FROM "User" WHERE "OAuth"."userId_old" = "User"."id";
+
+-- Drop old foreign key
+ALTER TABLE "OAuth" DROP CONSTRAINT "OAuth_userId_old_fkey";
+
+-- Drop old column
+ALTER TABLE "OAuth" DROP COLUMN "userId_old";
