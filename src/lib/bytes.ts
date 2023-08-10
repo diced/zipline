a9b05e8a9c335c7541005d02ae46d00a8a81b104
diced <pranaco2@gmail.com>
2023-08-09 23:27:52 -0700

    fix: bytes separator

--- src/lib/bytes.ts
+++ src/lib/bytes.ts
@@ -4,5 +4,5 @@ export function bytes(value: string): number;
 export function bytes(value: number, options?: BytesOptions): string;
 export function bytes(value: string | number, options?: BytesOptions): string | number {
   if (typeof value === 'string') return bytesFn(value);
-  return bytesFn(value, { ...options, unitSeparator: '' });
+  return bytesFn(value, { ...options, unitSeparator: ' ' });
 }
