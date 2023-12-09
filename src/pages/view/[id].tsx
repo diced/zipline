4519708d879f1bf196ce1fb13ebcbe323ff5dcfc
L7NEG <admin@l7neg.tk>
2023-12-09 18:29:13 +0200

    Fix Discord Embed Res Bug

--- src/pages/view/[id].tsx
+++ src/pages/view/[id].tsx
@@ -142,8 +142,6 @@ export default function EmbeddedFile({
             <meta property='og:video:url' content={`${host}/r/${file.name}`} />
             <meta property='og:video:secure_url' content={`${host}/r/${file.name}`} />
             <meta property='og:video:type' content={file.mimetype} />
-            <meta property='og:video:width' content='720' />
-            <meta property='og:video:height' content='480' />
           </>
         )}
         {file.mimetype.startsWith('audio') && (
