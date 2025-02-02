13ed70be6be1494686142771e9dc793a484ba3d6
Jayvin Hernandez <gogojayvin923@gmail.com>
2025-02-01 23:02:50 -0800

    fix: issue 659, it was the wrong statusCode to look for

--- src/server/routes/urls.ts
+++ src/server/routes/urls.ts
@@ -13,9 +13,9 @@ export default async function urlsRoute(this: FastifyInstance, req: FastifyReque
   });
   if (!url) return reply.notFound();
 
-  reply.redirect(url.destination);
-
   reply.postUrl(url);
+
+  reply.redirect(url.destination);
 }
 
 export async function urlsRouteOnResponse(
@@ -24,7 +24,7 @@ export async function urlsRouteOnResponse(
   reply: FastifyReply,
   done: () => void,
 ) {
-  if (reply.statusCode === 200) {
+  if (reply.statusCode === 302) {
     const { id } = req.params as { id: string };
 
     const url = await this.prisma.url.findFirst({
