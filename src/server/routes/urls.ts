942dbd3a78824a4bb5aaeef413cd185eeb966ba5
Jayvin Hernandez <gogojayvin923@gmail.com>
2025-02-06 17:53:57 -0800

    fix: just in case someone goes /upload(or /url)/dashboard

--- src/server/routes/urls.ts
+++ src/server/routes/urls.ts
@@ -3,7 +3,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 export default async function urlsRoute(this: FastifyInstance, req: FastifyRequest, reply: FastifyReply) {
   const { id } = req.params as { id: string };
   if (id === '') return reply.notFound();
-  else if (id === 'dashboard' && !this.config.features.headless)
+  else if (id === 'dashboard' && !this.config.features.headless && this.config.urls.route === '/')
     return this.nextServer.render(req.raw, reply.raw, '/dashboard');
 
   const url = await this.prisma.url.findFirst({
